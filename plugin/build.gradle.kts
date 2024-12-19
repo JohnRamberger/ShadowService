565f22fe5b2f62294f153f7b48b7e2febbc21f14
John Ramberger <rambergerjohn@gmail.com>
2024-12-18 22:58:50 -0500

    Update build.gradle.kts

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
     id("com.gradle.plugin-publish") version "1.3.0" // For publishing the plugin
 }
 
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+group = "com.jramberger"
 
 dependencies {
     implementation(project(":processor"))
@@ -18,13 +19,14 @@ tasks.test {
 }
 
 gradlePlugin {
+    website = "https://github.com/JohnRamberger/ShadowService"
+    vcsUrl = "https://github.com/JohnRamberger/ShadowService.git"
+
     plugins {
         create("shadow") {
             id = "com.jramberger.shadow"
             displayName = "ShadowService"
             description = "Generate AWS SAM configuration files directly from your code."
-            website = "https://github.com/JohnRamberger/ShadowService"
-            vcsUrl = "https://github.com/JohnRamberger/ShadowService.git"
             tags = listOf("aws", "sam", "shadow")
             implementationClass = "com.jramberger.shadow.plugin.ShadowPlugin"
         }
